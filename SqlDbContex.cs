0965dfb76826ddc39e3490930fedbaf0046e52a9
samarshadali <sam.arshadali@gmail.com>
2021-03-14 23:40:42 +0530

    Add files via upload

---
+++ SqlDbContex.cs
@@ -0,0 +1,15 @@
+﻿using DotNetCoreCURD.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace DotNetCoreCURD
+{
+    public class SqlDbContex : DbContext
+    {
+
+        public SqlDbContex(DbContextOptions<SqlDbContex> dbContextOptions) : base(dbContextOptions)
+        {
+
+        }
+        public DbSet<Student> Students { get; set; }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,50 @@
+using DotNetCoreCURD.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace DotNetCoreCURD
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddDbContextPool<SqlDbContex>(options =>
+options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddScoped<IStudentService, StudentService>();
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
